8007c44a3a219b3d6590a96dbdcda551ddbfcc42
Song-Li <lisong1013@gmail.com>
2020-03-29 13:49:12 -0400

    added solved

---
+++ README.md
@@ -0,0 +1,3 @@
+Run "python3.7 search.py" to get a list of links
+
+Run "python3.7 solve.py" to get the subject of the links
--- search.py
+++ search.py
@@ -2,5 +2,7 @@ from googlesearch import search
 
 query = "sched/ file changed site:lkml.org"
 
+fp = open('./result', 'w')
 for link in search(query, tbs='cdr%3A1%2Ccd_min%3A1%2F1%2F2019%2Ccd_max%3A3%2F27%2F2020'):
-    print(link)
+    fp.write("{}\n".format(link))
+fp.close()
