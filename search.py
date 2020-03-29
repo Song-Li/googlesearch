c59d3d57dc5d94509f649121c6c51ef6b4b4dc9d
Song-Li <lisong1013@gmail.com>
2020-03-29 13:43:24 -0400

    added solved

--- search.py
+++ search.py
@@ -1,7 +1,6 @@
 from googlesearch import search
 
-query = "Subject sched/ file changed site:lkml.org"
+query = "sched/ file changed site:lkml.org"
 
-for link in search(query, tbs='cdr%3A1%2Ccd_min%3A1%2F1%2F2020%2Ccd_max%3A3%2F27%2F2020'):
-    continue
+for link in search(query, tbs='cdr%3A1%2Ccd_min%3A1%2F1%2F2019%2Ccd_max%3A3%2F27%2F2020'):
     print(link)
